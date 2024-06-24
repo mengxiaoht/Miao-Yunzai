b0b25d4bda42e7f7fb3361acb4e1448b5e04403e
ningmengchongshui <916415899@qq.com>
2024-06-24 20:53:26 +0800

    feat: 修改启动方法

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-import { execSync, spawn } from 'child_process'
-execSync('tailwindcss -i ./src/input.css -o ./public/output.css')
+import { spawn } from 'child_process'
+const child1 = spawn(
+  'tailwindcss -i ./src/input.css -o ./public/output.css',
+  [],
+  {
+    shell: true,
+    stdio: 'inherit'
+  }
+)
 const argv = [...process.argv].splice(2)
 const argvs = argv.join(' ').replace(/(\S+\.js|\S+\.ts)/g, '')
-const child = spawn(
+const child2 = spawn(
   'node --no-warnings=ExperimentalWarning --loader ts-node/esm src/main.ts',
   argvs.split(' '),
   {
@@ -11,6 +18,7 @@ const child = spawn(
   }
 )
 process.on('SIGINT', () => {
-  if (child.pid) process.kill(child.pid)
+  if (child1.pid) process.kill(child1.pid)
+  if (child2.pid) process.kill(child2.pid)
   if (process.pid) process.exit()
 })
